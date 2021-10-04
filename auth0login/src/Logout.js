63ae11e30abbe9568542234593ed82ac7191f92a
jpromanonet <programming@jpromano.net>
2021-10-04 16:46:19 -0300

    Logout component

--- auth0login/src/Logout.js
+++ auth0login/src/Logout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-export const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+export const LogoutButton = () => {
+    const { logout } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Login</button>
+    return <button onClick={() => logout({ returnTo: window.location.origin})}>Login</button>
 }
