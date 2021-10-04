512e10989a96a3beeac20b50fc8f4bacc5501f7d
jpromanonet <programming@jpromano.net>
2021-10-04 16:48:48 -0300

    Done

--- auth0login/src/App.js
+++ auth0login/src/App.js
@@ -1,13 +1,23 @@
 import { LoginButton } from './Login';
+import { LogoutButton } from './Logout'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LoginButton />
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
       </header>
     </div>
   );
